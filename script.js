966061f902ec20a4e828dad9e088f7e72b69f1af
Luiz Eduardo Kuster de Azevedo <luiz.kuster.azevedo@escola.pr.gov.br>
2024-02-25 00:21:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 // Exemplo de interatividade simples com JavaScript
-document.addEventListener("DOMContentLoaded", function () {
-    const portfolioItems = document.querySelectorAll(".portfolio-item");
-    portfolioItems.forEach(item => {
-        item.addEventListener("mouseenter", function () {
-            this.classList.add("hovered");
-        });
-        item.addEventListener("mouseleave", function () {
-            this.classList.remove("hovered");
-        });
+//document.addEventListener("DOMContentLoaded", function () {
+   // const portfolioItems = document.querySelectorAll(".portfolio-item");
+   // portfolioItems.forEach(item => {
+       // item.addEventListener("mouseenter", function () {
+          //  this.classList.add("hovered");
+      //  });
+     //   item.addEventListener("mouseleave", function () {
+     //       this.classList.remove("hovered");
+     //   });
+   // });
+//});
+const buttons = document.querySelectorAll(".container-botoes button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const targetSection = event.target.textContent.toLowerCase();
+
+    // Scroll suave para a seção
+    document.querySelector("#" + targetSection).scrollIntoView({
+      behavior: "smooth",
     });
+
+    // Adicione um efeito de espera
+    const loader = document.createElement("div");
+    loader.classList.add("loader");
+    document.body.appendChild(loader);
+
+    setTimeout(() => {
+      loader.parentNode.removeChild(loader);
+    }, 1000);
+  });
 });
+
